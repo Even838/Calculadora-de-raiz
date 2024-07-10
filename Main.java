cb0de5ffe3af77e67bb2c82338c53d82adcaa134
Even-dev <samuca1234.oficial@gmail.com>
2024-07-10 14:22:49 -0300

    Create Main.java
    Arquivo principal

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" calculadora de raíz quedrada")
+;        for(int i=1;i<555;i++){
+            
+        
+        int number = scanner.nextInt(); // Correção aqui
+        
+        
+        int res = number*number;
+        
+        System.out.println(res);
+        }
+    }
+}
